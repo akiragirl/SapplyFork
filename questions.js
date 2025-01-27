bfc12026eac101efd880b8a11934989265e152df
akiragirl <okcool694.1@gmail.com>
2025-01-26 23:52:38 -0500

    changes

--- questions.js
+++ questions.js
@@ -1,26 +1,26 @@
 questions =  [
-	{"id": 0, "question": "Businesses should be free to do whatever they want.", "effects": {"right": 1}},
-	{"id": 1, "question": "Charity (assistance of people in need organized by private individuals or organizations) is a better system than social welfare (assistance of people in need organized by the state).", "effects": {"right": 1, "auth": -0.5}},
-	{"id": 2, "question": "Wages are always fair, as employers know best what a worker's labour is worth.", "effects": {"right": 1}},
+	{"id": 0, "question": "Businesses should be free to do almost whatever they want.", "effects": {"auth": -0.5}},
+	{"id": 1, "question": "Charity (assistance of people in need organized by private individuals or organizations) is a better system than social welfare (assistance of people in need organized by the state).", "effects": {"right": 0.5, "auth": -0.5}},
+	{"id": 2, "question": "Wages are always fair; we should trust all employers to determine the worth of a worker's labor.", "effects": {"right": 1}},
 	{"id": 3, "question": "Greediness is fine because it's just human nature.", "effects": {"right": 1}},
 	{"id": 4, "question": "\"Exploitation\" is an outdated term, as the struggles of 1800s capitalism don't exist anymore.", "effects": {"right": 1}},
 	{"id": 5, "question": "Socialism is bad for society.", "effects": {"right": 1}},
 	{"id": 6, "question": "Higher taxation of the wealthy is a bad idea; society would be better off without it.", "effects": {"right": 1}},
-	{"id": 7, "question": "The harder you work, the more you should progress up the social ladder.", "effects": {"right": 1}},
-	{"id": 8, "question": "Organizations and corporations can't always be trusted and should be regulated by the government.", "effects": {"right": -1, "auth": 0.5}},
+	{"id": 7, "question": "The harder you work, the more you should progress up the social ladder economically.", "effects": {"right": 1}},
+	{"id": 8, "question": "Organizations and corporations can't always be trusted and should be regulated by the government.", "effects": {"auth": 0.5}},
 	{"id": 9, "question": "An ideal government is one that provides for everyone.", "effects": {"right": -1}},
 	{"id": 10, "question": "The current welfare system should be expanded to further combat inequality.", "effects": {"right": -1}},
 	{"id": 11, "question": "Land should not be a commodity to be bought and sold.", "effects": {"right": -1}},
-	{"id": 12, "question": "Banking should be state-managed, not privately managed.", "effects": {"auth": 1}},
+	{"id": 12, "question": "Banking should mainly be state-managed, not privately managed, with the possible exception of some online banking services and cryptocurrency.", "effects": {"auth": 1}},
 	{"id": 13, "question": "Class is the primary division of society.", "effects": {"right": -1}},
 	{"id": 14, "question": "Economic inequality is too high in the world.", "effects": {"right": -1}},
 	{"id": 15, "question": "Sometimes, it is acceptable for the government to spy on some of its citizens to protect other citizens.", "effects": {"auth": 1}},
 	{"id": 16, "question": "Authority figures, if morally correct, are a good thing for society.", "effects": {"auth": 1}},
 	{"id": 17, "question": "A strong government is the bedrock of a utopian society.", "effects": {"auth": 1}},
-	{"id": 18, "question": "Large organizations, especially businesses, should be regulated by the government.", "effects": {"auth": 1}},
-	{"id": 19, "question": "Society should have bureaucracy.", "effects": {"auth": 1}},
+	{"id": 18, "question": "Large organizations, especially businesses, should be regulated by the government.", "effects": {"auth": 0.5}},
+	{"id": 19, "question": "Society should have governance.", "effects": {"auth": 1}},
 	{"id": 20, "question": "The government should strictly protect public safety.", "effects": {"auth": 1}},
-	//{"id": 21, "question": "In times of crisis, safety becomes more important than civil liberties.", "effects": {"auth": 1}}, // replace this
+	{"id": 21, "question": "Elections should be solely limited to candidates endorsed by existing state officials.", "effects": {"dem": -1}},
 	//{"id": 22, "question": "If you have nothing to hide, you have nothing to fear.", "effects": {"auth": 1}}, // replace this
 	//{"id": 23, "question": "The government should be less involved in the day to day life of its citizens.", "effects": {"auth": -1}}, // replace this
 	{"id": 24, "question": "An ideal society is one in which membership of the state is controlled by itself, not the people.", "effects": {"dem": -1}},
@@ -29,20 +29,21 @@ questions =  [
 	{"id": 27, "question": "The government should, at most, provide emergency services and law enforcement.", "effects": {"auth": -1}},
 	{"id": 28, "question": "Forget \"police reform\"; it needs to be abolished entirely.", "effects": {"auth": -0.5}},
 	{"id": 29, "question": "The state should not be involved in the education of our children whatsoever.", "effects": {"auth": -1}},
-	//{"id": 30, "question": "Two consenting individuals should be able to do whatever they want with each other, even if it makes me uncomfortable.", "effects": {"dem": 1}},
+	{"id": 30, "question": "The people should have continuous and constitutionally protected input in state leadership changes, not just via elections.", "effects": {"dem": 1}},
 	//{"id": 31, "question": "An individual's body is their own property, and they should be able to do anything they desire to it.", "effects": {"dem": 1}},
-	{"id": 32, "question": "A person should be able to preach whatever ideologies they like, even if those ideologies promote things such as unjust inequality or homophobia.", "effects": {"auth": -1}},
+	{"id": 32, "question": "A person should be able to preach whatever ideologies they like, even if those ideologies promote things such as unjust inequality or homophobia, as long as they don't act on them.", "effects": {"auth": -1}},
 	//{"id": 33, "question": "Nudism is perfectly natural.", "effects": {"dem": 1}},
 	//{"id": 34, "question": "Animals deserve certain universal rights.", "effects": {"dem": 1}},
 	//{"id": 35, "question": "Gender is a social construct, not a natural state of affairs.", "effects": {"dem": 1}}, // remove this
 	//{"id": 36, "question": "Laws based on cultural values, rather than ethical ones, aren't justice.", "effects": {"dem": 1}},
 	//{"id": 37, "question": "Autonomy of body extends even to minors, the mentally ill, and serious criminals.", "effects": {"dem": 1}},
-	//{"id": 38, "question": "Homosexuality is against my values.", "effects": {"dem": -1}}, // gotcha question; replace this
-	//{"id": 39, "question": "Transgender individuals should not be able to adopt children.", "effects": {"dem": -1}}, // gotcha question; replace this
+	//{"id": 38, "question": "Homosexuality is against my values.", "effects": {"dem": -1}}, // replace this
+	//{"id": 39, "question": "Transgender individuals should not be able to adopt children.", "effects": {"dem": -1}}, // replace this
 	//{"id": 40, "question": "Drugs are harmful and should be banned.", "effects": {"dem": -1}}, // replace this
-	{"id": 41, "question": "Exilation (temporary or permanent) should be considered for certain crimes.", "effects": {"auth": 0.5}},
+	{"id": 41, "question": "Exilation (temporary or permanent) should be considered for certain crimes.", "effects": {"auth": 1}},
 	//{"id": 42, "question": "Victimless crimes should still be punished.", "effects": {"dem": -1}}, // replace this
-	//{"id": 43, "question": "One cannot be moral without religion.", "effects": {"dem": -1}}, // gotcha question; replace this
+	//{"id": 43, "question": "One cannot be moral without religion.", "effects": {"dem": -1}}, // replace this
 	//{"id": 44, "question": "Parents should hold absolute power over their children, as they are older and more experienced.", "effects": {"auth": 1}}, // replace this
-	//{"id": 45, "question": "Multiculturalism is bad.", "effects": {"dem": -1}} // remove this; prog/con doesn't exist in this fork
+	//{"id": 45, "question": "Multiculturalism is bad.", "effects": {"dem": -1}}
+	{ "id": 42, "question": "Healthcare should be free.", "effects": { "right": -1 } },
 ];
