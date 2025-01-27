724ffabb95e529f53c48d7d762877a7eb07fca35
akiragirl <okcool694.1@gmail.com>
2025-01-27 00:56:57 -0500

    changes

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 questions =  [
-	{"id": 0, "question": "Businesses should be free to do almost whatever they want.", "effects": {"auth": -0.5}},
+	{"id": 0, "question": "Businesses should be free to do almost whatever they want.", "effects": {"auth": -0.2, "right": 0.5}},
 	{"id": 1, "question": "Charity (assistance of people in need organized by private individuals or organizations) is a better system than social welfare (assistance of people in need organized by the state).", "effects": {"right": 0.5, "auth": -0.5}},
 	{"id": 2, "question": "Wages are always fair; we should trust all employers to determine the worth of a worker's labor.", "effects": {"right": 1}},
 	{"id": 3, "question": "Greediness is fine because it's just human nature.", "effects": {"right": 1}},
@@ -7,28 +7,28 @@ questions =  [
 	{"id": 5, "question": "Socialism is bad for society.", "effects": {"right": 1}},
 	{"id": 6, "question": "Higher taxation of the wealthy is a bad idea; society would be better off without it.", "effects": {"right": 1}},
 	{"id": 7, "question": "The harder you work, the more you should progress up the social ladder economically.", "effects": {"right": 1}},
-	{"id": 8, "question": "Organizations and corporations can't always be trusted and should be regulated by the government.", "effects": {"auth": 0.5}},
+	{"id": 8, "question": "Organizations and corporations can't always be trusted and should be strictly regulated by the government.", "effects": {"auth": 0.2, "right": -0.5}},
 	{"id": 9, "question": "An ideal government is one that provides for everyone.", "effects": {"right": -1}},
 	{"id": 10, "question": "The current welfare system should be expanded to further combat inequality.", "effects": {"right": -1}},
 	{"id": 11, "question": "Land should not be a commodity to be bought and sold.", "effects": {"right": -1}},
-	{"id": 12, "question": "Banking should mainly be state-managed, not privately managed, with the possible exception of some online banking services and cryptocurrency.", "effects": {"auth": 1}},
+	{"id": 12, "question": "Banking should mainly be state-managed, not privately managed, with the possible exception of some online banking services and cryptocurrency.", "effects": {"auth": 1.2}},
 	{"id": 13, "question": "Class is the primary division of society.", "effects": {"right": -1}},
 	{"id": 14, "question": "Economic inequality is too high in the world.", "effects": {"right": -1}},
 	{"id": 15, "question": "Sometimes, it is acceptable for the government to spy on some of its citizens to protect other citizens.", "effects": {"auth": 1}},
 	{"id": 16, "question": "Authority figures, if morally correct, are a good thing for society.", "effects": {"auth": 1}},
 	{"id": 17, "question": "A strong government is the bedrock of a utopian society.", "effects": {"auth": 1}},
 	{"id": 18, "question": "Large organizations, especially businesses, should be regulated by the government.", "effects": {"auth": 0.5}},
-	{"id": 19, "question": "Society should have governance.", "effects": {"auth": 1}},
-	{"id": 20, "question": "The government should strictly protect public safety.", "effects": {"auth": 1}},
+	{"id": 19, "question": "Society should have strong governance.", "effects": {"auth": 1}},
+	{"id": 20, "question": "The government should strictly prohibit acts that go against public safety.", "effects": {"auth": 0.5}},
 	{"id": 21, "question": "Elections should be solely limited to candidates endorsed by existing state officials.", "effects": {"dem": -1}},
 	//{"id": 22, "question": "If you have nothing to hide, you have nothing to fear.", "effects": {"auth": 1}}, // replace this
-	//{"id": 23, "question": "The government should be less involved in the day to day life of its citizens.", "effects": {"auth": -1}}, // replace this
+	{"id": 23, "question": "The government should be less involved in the day to day life of its citizens.", "effects": {"auth": -1}},
 	{"id": 24, "question": "An ideal society is one in which membership of the state is controlled by itself, not the people.", "effects": {"dem": -1}},
-	{"id": 25, "question": "Authoritarianism is always bad, full stop. I don't care if it's \"democratic\".", "effects": {"auth": -1}},
-	{"id": 26, "question": "The state must be eliminated (or reduced in terms of power and/or size) in order for the people to have freedom.", "effects": {"auth": -1}},
+	{"id": 25, "question": "Authoritarianism is always bad, full stop. I don't care if it's \"democratic\".", "effects": {"auth": -2}},
+	{"id": 26, "question": "The state should have less power.", "effects": {"auth": -1}},
 	{"id": 27, "question": "The government should, at most, provide emergency services and law enforcement.", "effects": {"auth": -1}},
-	{"id": 28, "question": "Forget \"police reform\"; it needs to be abolished entirely.", "effects": {"auth": -0.5}},
-	{"id": 29, "question": "The state should not be involved in the education of our children whatsoever.", "effects": {"auth": -1}},
+	{"id": 28, "question": "The police should be defunded, not reformed.", "effects": {"auth": -0.2}},
+	{"id": 29, "question": "The state should not be involved in the education system.", "effects": {"auth": -1}},
 	{"id": 30, "question": "The people should have continuous and constitutionally protected input in state leadership changes, not just via elections.", "effects": {"dem": 1}},
 	//{"id": 31, "question": "An individual's body is their own property, and they should be able to do anything they desire to it.", "effects": {"dem": 1}},
 	{"id": 32, "question": "A person should be able to preach whatever ideologies they like, even if those ideologies promote things such as unjust inequality or homophobia, as long as they don't act on them.", "effects": {"auth": -1}},
@@ -46,4 +46,8 @@ questions =  [
 	//{"id": 44, "question": "Parents should hold absolute power over their children, as they are older and more experienced.", "effects": {"auth": 1}}, // replace this
 	//{"id": 45, "question": "Multiculturalism is bad.", "effects": {"dem": -1}}
 	{ "id": 42, "question": "Healthcare should be free.", "effects": { "right": -1 } },
+	{ "id": 43, "question": "The collective public should have the power to remove state officials at any time.", "effects": { "dem": 1 } },
+	{ "id": 44, "question": "A person should be a family member of an existing state official in order to be eligible to become one.", "effects": { "dem": -1 } },
+	{ "id": 45, "question": "The state should have the ability to invite a person to become a state official without any say from the people.", "effects": { "dem": -0.5 } },
+	{ "id": 46, "question": "Capitalism sucks.", "effects": { "right": -1 } }
 ];
